586578d60b8118dbbd2fff63aac874fdc5fb9397
JungJay <keunhee0920@gmail.com>
2025-04-16 12:22:59 +0900

    [25.04.16] 카카오 코테 문제 풀이

--- java/algorithm/src/main/java/algorithm_practice/fifth_week/KaKao_03.java
+++ java/algorithm/src/main/java/algorithm_practice/fifth_week/KaKao_03.java
@@ -4,13 +4,57 @@ public class KaKao_03 {
     public static void main(String[] args) {
         String balancedParentheses = "()))((()";
 
+        System.out.println("정답 = ()(())() / 현재 풀이 값 = " + getCorrectParentheses(balancedParentheses));
         System.out.println("정답 = (((()))) / 현재 풀이 값 = " + getCorrectParentheses(")()()()("));
         System.out.println("정답 = ()()( / 현재 풀이 값 = " + getCorrectParentheses("))()("));
         System.out.println("정답 = ((((()())))) / 현재 풀이 값 = " + getCorrectParentheses(")()()()(())("));
     }
 
-    public static String getCorrectParentheses(String str) {
+    public static String getCorrectParentheses(String p) {
+        if (p.isEmpty()) return "";
 
-        return "";
+        // 1. u, v 분리
+        int idx = splitIndex(p);
+        String u = p.substring(0, idx);
+        String v = p.substring(idx);
+
+        if (isCorrect(u)) {
+            return u + getCorrectParentheses(v);
+        } else {
+            StringBuilder sb = new StringBuilder();
+            sb.append("(");
+            sb.append(getCorrectParentheses(v));
+            sb.append(")");
+            sb.append(reverse(u.substring(1, u.length() - 1)));
+            return sb.toString();
+        }
+    }
+
+    private static int splitIndex(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') count++;
+            else count--;
+            if (count == 0) return i + 1;
+        }
+        return s.length();
+    }
+
+    private static boolean isCorrect(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') count++;
+            else count--;
+            if (count < 0) return false;
+        }
+        return count == 0;
+    }
+
+    private static String reverse(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            sb.append(c == '(' ? ')' : '(');
+        }
+        return sb.toString();
     }
 }

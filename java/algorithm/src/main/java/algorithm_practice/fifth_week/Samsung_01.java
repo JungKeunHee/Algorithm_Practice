46965b5940eff3395417fc88fbdc19e7bfb329a0
JungJay <keunhee0920@gmail.com>
2025-05-07 08:40:26 +0900

    [25.05.07] 알고리즘 공부

--- java/algorithm/src/main/java/algorithm_practice/fifth_week/Samsung_01.java
+++ java/algorithm/src/main/java/algorithm_practice/fifth_week/Samsung_01.java
@@ -30,7 +30,7 @@ public class Samsung_01 {
         // 체스판 위 말 스택 초기화
         board = new ArrayList[N][N];
         for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
+            for (int j = 0; j < N; j++) 
                 board[i][j] = new ArrayList<>();
 
         for (int i = 0; i < K; i++) {
@@ -56,7 +56,7 @@ public class Samsung_01 {
         int r = h.row;
         int c = h.col;
         int d = h.dir;
- 
+
         int nr = r + dr[d];
         int nc = c + dc[d]; 
 
